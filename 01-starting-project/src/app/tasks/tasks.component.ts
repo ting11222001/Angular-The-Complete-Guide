c8333746c7a98e25a608eef956821ff8ce1d7bf8
Li-Ting Liao <39208708+ting11222001@users.noreply.github.com>
2026-07-26 22:53:04 +0930

    Move the dummy tasks data to a dedicated constant file

--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component, input, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { DUMMY_TASKS } from '../dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -11,31 +12,7 @@ import { TaskComponent } from './task/task.component';
 export class TasksComponent {
   userId = input.required<String>();
   name = input.required<String>();
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
+  tasks = DUMMY_TASKS;
 
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId());
